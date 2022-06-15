67505401bf49840ef6487299bd7b9a6892c938fa
AndyChienn <andy2002chien@hotmail.com.tw>
2022-06-16 00:33:25 +0800

    feat: update restaurantSeeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -8,8 +8,6 @@ const User = require('../user')
 const db = require('../../config/mongoose')
 const restaurantList = require('../../restaurant.json').results
 
-
-
 const SEED_USERS = [
   {
     name: 'USER1',
@@ -23,7 +21,6 @@ const SEED_USERS = [
     password: '12345678',
     restaurantId: [4, 5, 6]
   }
-
 ]
 
 db.once('open', () => {
@@ -31,20 +28,20 @@ db.once('open', () => {
     return bcrypt
       .genSalt(10)
       .then(salt => bcrypt.hash(SEED_USER.password, salt))
-      .then(hash => User.create({
-        name: SEED_USER.name,
-        email: SEED_USER.email,
-        password: hash
-      }))
-      .then(user => {
-        const userId = user._id
-        const addRestaurants = restaurantList.filter(restaurant => {
-          return SEED_USER.restaurantId.includes(restaurant._id)
+      .then(hash => {
+        return User.create({
+          name: SEED_USER.name,
+          email: SEED_USER.email,
+          password: hash
+        }).then((user) => {
+          const addRestaurants = restaurantList.filter(restaurant => {
+            return SEED_USER.restaurantId.includes(restaurant.id)
+          })
+          return Promise.all(Array.from(addRestaurants, addRestaurant => {
+            addRestaurant.userId = user._id
+            return Restaurant.create(addRestaurant)
+          }))
         })
-        return Promise.all(Array.from(addRestaurants, restaurant => {
-          restaurant.userId = userId
-          return Restaurant.create(restaurant)
-        }))
       })
   }))
     .then(() => {
@@ -54,8 +51,191 @@ db.once('open', () => {
     .catch(err => console.log(err))
 })
 
+//以下為學習後面教案所更新的各種用法 SEED_USERS的設定也些許不同
+// const SEED_USERS = [
+//   {
+//     name: 'USER1',
+//     email: 'user1@example.com',
+//     password: '12345678',
+//   },
+//   {
+//     name: 'USER2',
+//     email: 'user2@example.com',
+//     password: '12345678',
+//   }
+// ]
+
+//method: async await
+
+// db.once('open', async () => {
+//   await Promise.all(
+//     SEED_USERS.map(async (SEED_USER, SEED_USER_index) => {
+//       const salt = await bcrypt.genSalt(10)
+//       const hash = await bcrypt.hash(SEED_USER.password, salt)
+//       const user = await User.create({ name: SEED_USER.name, email: SEED_USER.email, password: hash })
+//       console.log('user created')
+//       const userRestaurant = []
+//       restaurantList.forEach((restaurant, rest_index) => {
+//         if (rest_index >= 3 * SEED_USER_index && rest_index < 3 * (SEED_USER_index + 1)) {
+//           restaurant.userId = user._id
+//           userRestaurant.push(restaurant)
+//         }
+//       })
+//       await Restaurant.create(userRestaurant)
+//       console.log('restaurant created')
+//     })
+//   )
+//   console.log('all created')
+//   process.exit()
+// })
+
+// method: Promise.all
 // db.once('open', () => {
-//   Promise.all(Array.from(SEED_USERS, SEED_USER => {
+//   Promise.all(
+//     SEED_USERS.map((SEED_USER, SEED_USER_index) => {
+//       return bcrypt.genSalt(10)
+//         .then(salt => bcrypt.hash(SEED_USER.password, salt))
+//         .then(hash => {
+//           return User.create({
+//             name: SEED_USER.name,
+//             email: SEED_USER.email,
+//             password: hash
+//           }).then((SEED_USER) => {
+//             console.log('users created')
+//             const userRestaurant = []
+//             restaurantList.forEach((restaurant, rest_index) => {
+//               if (rest_index >= 3 * SEED_USER_index && rest_index < 3 * (SEED_USER_index + 1)) {
+//                 restaurant.userId = SEED_USER._id
+//                 userRestaurant.push(restaurant)
+//               }
+//             })
+//             return Restaurant.create(userRestaurant)
+//           })
+//         })
+//     })
+//   ).then(() => {
+//     console.log('all created!')
+//     process.exit()
+//   })
+// })
+
+
+
+// method: promise
+
+// db.once('open', () => {
+//   new Promise((resolve, _reject) => {
+//     for (const [SEED_USER_index, SEED_USER] of SEED_USERS.entries()) {
+//       bcrypt.genSalt(10)
+//         .then(salt => bcrypt.hash(SEED_USER.password, salt))
+//         .then(hash => {
+//           return User.create({
+//             name: SEED_USER.name,
+//             email: SEED_USER.email,
+//             password: hash
+//           }).then((SEED_USER) => {
+//             console.log('user created')
+//             console.log('SEED_USER', SEED_USER)
+//             const userRestaurant = []
+//             restaurantList.forEach((restaurant, rest_index) => {
+//               if (rest_index >= 3 * SEED_USER_index && rest_index < 3 * (SEED_USER_index + 1)) {
+//                 restaurant.userId = SEED_USER._id
+//                 userRestaurant.push(restaurant)
+//               }
+//             })
+//             return Restaurant.create(userRestaurant)
+//           })
+//         })
+//         .then(() => {
+//           console.log('restaurants created!')
+
+//           if (SEED_USER_index >= SEED_USERS.length - 1) {
+//             resolve()
+//           }
+//         })
+//     }
+//   }).then(() => {
+//     console.log('all created!')
+//     process.exit()
+//   })
+
+// })
+
+// db.once('open', () => {
+//   new Promise((resolve, _reject) => {
+//     for (const [SEED_USER_index, SEED_USER] of SEED_USERS.entries()) {
+//       bcrypt.genSalt(10)
+//         .then(salt => bcrypt.hash(SEED_USER.password, salt))
+//         .then(hash => {
+//           return User.create({
+//             name: SEED_USER.name,
+//             email: SEED_USER.email,
+//             password: hash
+//           })
+//         })
+//         .then((SEED_USER) => {
+//           console.log('user created')
+//           console.log('SEED_USER', SEED_USER)
+//           const userRestaurant = []
+//           restaurantList.forEach((restaurant, rest_index) => {
+//             if (rest_index >= 3 * SEED_USER_index && rest_index < 3 * (SEED_USER_index + 1)) {
+//               restaurant.userId = SEED_USER._id
+//               userRestaurant.push(restaurant)
+//             }
+//           })
+//           return Restaurant.create(userRestaurant)
+//         }).then(() => {
+//           console.log('restaurants created!')
+//           if (SEED_USER_index >= SEED_USERS.length - 1) {
+//             resolve()
+//           }
+//         })
+//     }
+//   }).then(() => {
+//     console.log('all created!')
+//     process.exit()
+//   })
+
+// })
+
+
+
+//method: callback
+
+// db.once('open', () => {
+//   for (const [SEED_USER_index, SEED_USER] of SEED_USERS.entries()) {
+//     bcrypt.genSalt(10)
+//       .then(salt => bcrypt.hash(SEED_USER.password, salt))
+//       .then(hash => {
+//         User.create({
+//           name: SEED_USER.name,
+//           email: SEED_USER.email,
+//           password: hash
+//         },
+//           (err, SEED_USER) => {
+//             console.log('SEED_USER created')
+//             const userRestaurant = []
+//             restaurantList.forEach((restaurant, res_index) => {
+//               if (res_index >= 3 * SEED_USER_index && res_index < 3 * (SEED_USER_index + 1)) {
+//                 restaurant.userId = SEED_USER._id
+//                 userRestaurant.push(restaurant)
+//               }
+//             })
+//             Restaurant.create(userRestaurant, () => {
+//               console.log('restaurants created!')
+//               // console.log(userRestaurant)
+//               if (SEED_USER_index >= SEED_USERS.length - 1) {
+//                 console.log('all created!')
+//                 process.exit()
+//               }
+//             })
+//           })
+//       })
+//   }
+// })
+
+// db.once('open', () => {
+//   return Promise.all(Array.from(SEED_USERS, SEED_USER => {
 //     return bcrypt
 //       .genSalt(10)
 //       .then(salt => bcrypt.hash(SEED_USER.password, salt))
@@ -65,61 +245,20 @@ db.once('open', () => {
 //         password: hash
 //       }))
 //       .then(user => {
-
 //         const userId = user._id
-//         const seedRestaurants = restaurantList.filter(restaurant => {
-//           return SEED_USER.restaurantId.includes(restaurant.id)
-
+//         const addRestaurants = restaurantList.filter(restaurant => {
+//           return SEED_USER.restaurantId.includes(restaurant._id)
 //         })
-//         return Promise.all(Array.from(seedRestaurants, restaurant => {
+//         return Promise.all(Array.from(addRestaurants, restaurant => {
 //           restaurant.userId = userId
 //           return Restaurant.create(restaurant)
 //         }))
 //       })
-//   })
-//   )
-//     .then(() => {
-//       console.log('Seeds Done.')
-//       process.exit()
-//     })
-// })
-      // .then(SEED_USER => {
-      //   return Promise.all(Array.from(SEED_USER.restaurants, index => {
-      //     const restaurant = restaurants[index]
-      //     Object.assign(restaurant, { userId: user._id })
-      //     return Restaurant.create(restaurant)
-
-      //   }))
-      // })
-
-
-
-// db.once('open', () => {
-//   return Promise.all(SEED_USERS.map(user => {
-//     const resList = user.restaurantsForUser
-//     User.create({
-//       name: user.name,
-//       email: user.email,
-//       password: bcrypt.hashSync(user.password, bcrypt.genSaltSync(10))
-//     })
-//       .then(user => {
-//         return Promise.all(resList.map(restaurant => {
-//           Restaurant.create({
-//             name: restaurant.name,
-//             name_en: restaurant.name_en,
-//             category: restaurant.category,
-//             image: restaurant.image,
-//             location: restaurant.location,
-//             phone: restaurant.phone,
-//             google_map: restaurant.google_map,
-//             rating: restaurant.rating,
-//             description: restaurant.description,
-//             userId: user._id
-//           })
-//         }))
-//       })
 //   }))
 //     .then(() => {
-//       console.log('done!')
+//       console.log('Seeds done.')
 //       process.exit()
 //     })
+//     .catch(err => console.log(err))
+// })
+
